51445b5bce02f39cceab1c3023154237b07a7cb4
OrchisamaDas <orchisama.das@gmail.com>
2016-07-15 23:53:09 +0530

    Now works as a function computing Pitch Class Profile

--- chromagram.py
+++ chromagram.py
@@ -6,11 +6,8 @@ This script computes 12 dimensional chromagram for chord detection
 """
 
 from __future__ import division
-import os
-from scipy.signal import hamming,hann
-from scipy.io.wavfile import read
-from scipy.fftpack import fft, fftshift
-from scipy import sparse
+from scipy.signal import hamming
+from scipy.fftpack import fft
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -18,8 +15,19 @@ def nearestPow2(inp):
     power = np.ceil(np.log2(inp))
     return 2**power
 
+"""Function to calculcate Harmonic Power Spectrum from DFT"""
+def HPS(dft,M):
+
+	hps_len = int(np.ceil(np.size(dft)/(2**M)))
+ 	hps = np.ones(hps_len)
+ 	for n in range(hps_len):
+ 		for m in range(M+1): 
+ 			hps[n] *= np.absolute(dft[(2**m)*n])
+ 	return hps	
+
+
 """Function to compute CQT using sparse matrix multiplication, Brown and Puckette 1992- fast"""	
-def CQT_fast(x,fs,bins,fmin,fmax):
+def CQT_fast(x,fs,bins,fmin,fmax,M):
 
 	threshold = 0.0054 #for Hamming window
 	K = int(bins*np.ceil(np.log2(fmax/fmin)))
@@ -38,11 +46,16 @@ def CQT_fast(x,fs,bins,fmin,fmax):
 		specKernel[np.where(np.abs(specKernel) <= threshold)] = 0
 		if k == K-1:
 			sparKernel = specKernel
-		sparKernel = np.vstack((specKernel, sparKernel))
+		else:
+			sparKernel = np.vstack((specKernel, sparKernel))
 	
 	sparKernel = np.transpose(np.conjugate(sparKernel))/nfft
 	ft = fft(x,nfft)
 	cqt = np.dot(ft, sparKernel)
+	ft = fft(x,nfft*(2**M))
+	#calculate harmonic power spectrum
+	# harm_pow = HPS(ft,M)
+	# cqt = np.dot(harm_pow, sparKernel)
 	return cqt
 
 
@@ -57,7 +70,6 @@ def CQT_slow(x, fs, bins, fmin, fmax):
 		fk = (2**(k/bins))*fmin
 		N = int(np.round(Q*fs/fk))
 		arr = -2*np.pi*1j*Q*np.arange(N)/N 
-		#cqt[k] = np.sum(x[:N] * np.dot(hamming(N), np.exp(arr)))/N
 		cqt[k] = np.dot(x[:N], np.transpose(hamming(N) * np.exp(arr)))/N 
 	return cqt
 
@@ -70,63 +82,60 @@ def PCP(cqt,bins,M):
 	return CH
 
 
-"""call functions here"""
-directory = os.getcwd() + '/test_chords/';
-fname = 'Grand Piano - Fazioli - major C middle.wav';
-(fs,s) = read(directory + fname)
-
+def compute_chroma(s,fs):
 #downsample sampling frequency to 11025Hz
-x = s[::4]
-x = x[:,1]
-fs = int(fs/4)
+	x = s[::4]
+	x = x[:,1]
+	fs = int(fs/4)
 
-#framing audio, window length = 8192, hop size = 1024
-nfft = 8192
-hop_size = 1024
-nFrames = int(np.round(len(x)/(nfft-hop_size)))
-#zero padding to make signal length long enough to have nFrames
-x = np.append(x, np.zeros(nfft))
-xFrame = np.empty((nfft, nFrames))
-start = 0    
-for n in range(nFrames):
-	xFrame[:,n] = x[start:start+nfft] 
-	start = start + nfft - hop_size 
+	#framing audio, window length = 8192, hop size = 1024
+	nfft = 8192
+	hop_size = 1024
+	nFrames = int(np.round(len(x)/(nfft-hop_size)))
+	#zero padding to make signal length long enough to have nFrames
+	x = np.append(x, np.zeros(nfft))
+	xFrame = np.empty((nfft, nFrames))
+	start = 0    
+	for n in range(nFrames):
+		xFrame[:,n] = x[start:start+nfft] 
+		start = start + nfft - hop_size 
 
+	fmin = 96
+	fmax = 5250
+	bins = 12
+	M = 3
+	nOctave = np.int32(np.ceil(np.log2(fmax/fmin)))
+	CH = np.empty((bins,nFrames))
+	for n in range(nFrames):
+		#compute constant Q transform
+		cqt_fast = CQT_fast(xFrame[:,n],fs,bins,fmin,fmax,M)
+		plt.figure(1)
+		plt.plot(np.absolute(cqt_fast))
+		#compute pitch class profile
+		CH[:,n] = PCP(np.absolute(cqt_fast), bins, nOctave)
+		plt.figure(2)
+		plt.plot(CH[:,n])
 
-#compute constant Q transform for each frame
-fmin = 96
-fmax = 5250
-bins = 12
-nOctave = np.int32(np.ceil(np.log2(fmax/fmin)))
-CH = np.empty((bins,nFrames))
-for n in range(nFrames):
-	cqt_fast = CQT_fast(xFrame[:,n],fs,bins,fmin,fmax)
-	#plot constant Q transform calcuated two ways
 	plt.figure(1)
-	plt.plot(np.absolute(cqt_fast))
-	#compute pitch class profile
-	CH[:,n] = PCP(np.absolute(cqt_fast), bins, nOctave)
+	plt.xlabel('Bin Number')
+	plt.ylabel('Magnitude of Q transform')
+	plt.title('CQT')
+
 	plt.figure(2)
-	plt.plot(CH[:,n])
+	notes = ['G','G#','A','A#','B','C','C#','D','D#','E','F','F#']
+	plt.xticks(np.arange(bins),notes)
+	plt.title('Pitch Class Profile')
+	plt.xlabel('Note')
 
-plt.figure(1)
-plt.xlabel('Bin Number')
-plt.ylabel('Magnitude of Q transform')
-plt.title('CQT')
+	#averaged and normalized chromagram
+	chroma = np.mean(CH,axis = 1)
+	chroma /= np.max(chroma)
+	plt.figure(3)
+	plt.plot(chroma)
+	plt.xticks(np.arange(bins),notes)
+	plt.title('Averaged PCP')
+	plt.xlabel('Note')
+	plt.show()
 
-plt.figure(2)
-notes = ['G','G#','A','A#','B','C','C#','D','D#','E','F','F#']
-plt.xticks(np.arange(bins),notes)
-plt.title('Pitch Class Profile')
-plt.xlabel('Note')
-
-#averaged and normalized chromagram
-chroma = np.mean(CH,axis = 1)
-chroma /= np.max(chroma)
-plt.figure(3)
-plt.plot(chroma)
-plt.xticks(np.arange(bins),notes)
-plt.title('Pitch Class Profile')
-plt.xlabel('Note')
-plt.show()
+	return chroma
 
