960ce4603429cf6f4e803bac19c9f790026d81f3
OrchisamaDas <orchisama.das@gmail.com>
2016-07-17 19:18:28 +0530

    Fixed some issues - works well on minor chords, OK on major chords

--- hmm.py
+++ hmm.py
@@ -10,20 +10,17 @@ import numpy as np
 import json
 
 
-def assign_vals(indices):
-	x = np.zeros(12)
-	x[indices[0]] = 0.8
-	x[indices[1]] = 0.6
-	x[indices[2]] = 0.8
-	return x
-
 """calculates multivariate gaussian matrix from mean and covariance matrices"""
 def multivariate_gaussian(x, meu, cov):
-	#normalize chroma vector
-	x = x/np.max(x)
+	
 	det = np.linalg.det(cov)
-	val = -0.5 * np.dot(np.dot((x-meu).T, np.linalg.inv(cov)), (x-meu))
-	val /= np.sqrt(((2*np.pi)**12)*det)	
+	val = np.exp(-0.5 * np.dot(np.dot((x-meu).T, np.linalg.inv(cov)), (x-meu)))
+	try:
+		val /= np.sqrt(((2*np.pi)**12)*det)
+	except:
+		print 'Matrix is not positive, semi-definite'
+	if np.isnan(val):
+		val = np.finfo(float).eps
 	return val
 
 
@@ -35,7 +32,7 @@ def initialize(chroma, templates, nested_cof):
 	PI = np.ones(24)/24
 
 	"""initialising A based on nested circle of fifths"""
-	eps = 0.1
+	eps = 0.01
 	A = np.empty((24,24))
 	for chord in chords:
 		ind = nested_cof.index(chord)
@@ -46,7 +43,7 @@ def initialize(chroma, templates, nested_cof):
 			A[ind][t] = (abs(12-i)+eps)/(144 + 24*eps)
 			t += 1
 
-
+	
 	"""initialising based on tonic triads - Mean matrix; Tonic with dominant - 0.8,
 	tonic with mediant 0.6 and mediant-dominant 0.8, non-triad diagonal	elements 
 	with 0.2 - covariance matrix"""
@@ -56,23 +53,43 @@ def initialize(chroma, templates, nested_cof):
 	meu_mat = np.zeros((24,12))
 	cov_mat = np.zeros((24,12,12))
 	meu_mat = np.array(templates)
+	offset = 0
 
 	for i in range(24):
-		args = np.where(meu_mat[i,:] == 1)
-		#filling tonic triad
-		for arg in np.nditer(args):
-			ind = np.where(meu_mat[arg,:] == 1)
-			cov_mat[i,arg,:] = assign_vals(ind[0])
-		#filling diagonal elements
+		if i == 12:
+			offset = 0
+		tonic = offset
+		if i<12:
+			mediant = (tonic + 4)%12
+		else:
+			mediant = (tonic + 3)%12
+		dominant = (tonic+7)%12
+
+		#weighted diagonal
+		cov_mat[i,tonic,tonic] = 0.8
+		cov_mat[i,mediant,mediant] = 0.6
+		cov_mat[i,dominant,dominant] = 0.8
+
+		#off-diagonal - matrix not positive semidefinite, hence determinant is negative
+		# for n in [tonic,mediant,dominant]:
+		# 	for m in [tonic, mediant, dominant]:
+		# 		if (n is tonic and m is mediant) or (n is mediant and m is tonic):
+		# 			cov_mat[i,n,m] = 0.6
+		# 		else:
+		# 			cov_mat[i,n,m] = 0.8
+
+		#filling non zero diagonals
 		for j in range(12):
-			if cov_mat[i,j,j] == 0:
-				cov_mat[i,j,j] = 0.2
-			
+	 		if cov_mat[i,j,j] == 0:
+	 			cov_mat[i,j,j] = 0.2
+	 	offset += 1
+	
 
 	"""observation matrix B is a multivariate Gaussian calculated from mean vector and 
 	covariance matrix"""
-	for n in range(24):
-		for m in range(nFrames):
+
+	for m in range(nFrames):
+		for n in range(24):
 			B[n,m] = multivariate_gaussian(chroma[:,m], meu_mat[n,:],cov_mat[n,:,:])
 
 	return (PI,A,B)
@@ -80,6 +97,7 @@ def initialize(chroma, templates, nested_cof):
 
 
 """Viterbi algorithm to find Path with highest probability - dynamic programming"""
+
 def viterbi(PI,A,B):
 	(nrow, ncol) = np.shape(B)
 	path = np.zeros((nrow, ncol))
@@ -98,6 +116,7 @@ def viterbi(PI,A,B):
 
 
 """read from JSON file to get chord templates"""
+
 with open('chord_templates.json', 'r') as fp:
 	templates_json = json.load(fp)
 
@@ -110,9 +129,11 @@ for chord in chords:
 
 """read audio and compute chromagram"""
 directory = os.getcwd() + '/test_chords/';
-fname = 'Grand Piano - Fazioli - major A middle.wav';
+fname = 'Grand Piano - Fazioli - minor chords - Am higher.wav';
 (fs,s) = read(directory + fname)
 
+
+#reduce sample rate and convert to mono
 x = s[::4]
 x = x[:,1]
 fs = int(fs/4)
@@ -121,17 +142,23 @@ fs = int(fs/4)
 nfft = 8192
 hop_size = 1024
 nFrames = int(np.round(len(x)/(nfft-hop_size)))
+
 #zero padding to make signal length long enough to have nFrames
 x = np.append(x, np.zeros(nfft))
 xFrame = np.empty((nfft, nFrames))
 start = 0   
 chroma = np.empty((12,nFrames)) 
 timestamp = np.zeros(nFrames)
+
 #compute PCP
 for n in range(nFrames):
 	xFrame[:,n] = x[start:start+nfft] 
 	start = start + nfft - hop_size 
 	chroma[:,n] = compute_chroma(xFrame[:,n],fs)
+	if  np.all(chroma[:,n] == 0):
+		chroma[:,n] = np.finfo(float).eps
+	else:
+		chroma[:,n] /= np.max(np.absolute(chroma[:,n]))
 	timestamp[n] = n*(nfft-hop_size)/fs 
 
 
@@ -147,9 +174,20 @@ for i in range(nFrames):
 final_chords = []
 indices = np.argmax(path,axis=0)
 final_states = np.zeros(nFrames)
+
+
+#find no chord zone
+set_zero = np.where(np.max(path,axis=0) < 0.3*np.max(path))[0]
+if np.size(set_zero) is not 0:
+	indices[set_zero] = -1
+
+#identify chords
 for i in range(nFrames):
-	final_states[i] = states[indices[i],i]
-	final_chords.append(chords[int(final_states[i])])
+	if indices[i] == -1:
+		final_chords.append('NC')
+	else:
+		final_states[i] = states[indices[i],i]
+		final_chords.append(chords[int(final_states[i])])
 
 print 'Time(s)','Chords'
 for i in range(nFrames):
